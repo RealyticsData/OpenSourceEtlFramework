e5c1c67eacfd454297f6e8f8d8b37d66dc49e4c2
JonathanSzeto <Jonathan@JonathanSzeto.com>
2024-04-25 16:05:38 -0400

    Attached ECS policy to service account

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -8,6 +8,10 @@ resource "aws_iam_group" "microservice_testers" {
   path = "/OpenSourceEtlFramework/"
 }
 
+data "aws_iam_policy" "AmazonEC2FullAccess" {
+  arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
+
 resource "aws_iam_role" "container_service_account" {
   name   = "OpenSourceEtlFrameworkServiceAccount"
 
@@ -15,13 +19,18 @@ resource "aws_iam_role" "container_service_account" {
     Version: "2012-10-17",
     Statement = [
       {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Sid       = ""
         Principal = {
           Service = "ec2.amazonaws.com"
         }
       },
     ]
   })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_ecs_policy" {
+  role       = aws_iam_role.container_service_account.name
+  policy_arn = data.aws_iam_policy.AmazonEC2FullAccess.arn
 }
