b2e98c56829e5227a2d851d43d49107fba15ea95
JonathanSzeto <Jonathan@JonathanSzeto.com>
2024-04-25 14:51:49 -0400

    Fixed resource declarations

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,19 +1,13 @@
-module "iam_developers" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-group-with-policies"
-  version = "5.39.0"
-  name    = "OpenSourceEtlFrameworkDevelopers"
+resource "aws_iam_group" "microservice_developers" {
+  name = "MicroserviceDevelopers"
+  path = "/OpenSourceEtlFramework/"
 }
 
-module "iam_testers" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-group-with-policies"
-  version = "5.39.0"
-  name    = "OpenSourceEtlFrameworkTesters"
+resource "aws_iam_group" "microservice_testers" {
+  name = "MicroserviceTesters"
+  path = "/OpenSourceEtlFramework/"
 }
 
-module "iam_container_role" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-roles"
-  version = "5.39.0"
-  
-  create_poweruser_role = true
-  poweruser_role_name   = "OpenSourceEtlFrameworkServiceAccount"
+resource "aws_iam_role" "container_service_account" {
+  name   = "OpenSourceEtlFrameworkServiceAccount"
 }
--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,11 +1,11 @@
 output "developers_group_arn" {
-  value = iam_developers.group_arn
+  value = resource.aws_iam_group.microservice_developers.arn
 }
 
 output "testers_group_arn" {
-  value = iam_testers.group_arn
+  value = resource.aws_iam_group.microservice_testers.arn
 }
 
 output "container_role_arn" {
-  value = iam_container_role.iam_role_arn
+  value = resource.aws_iam_role.container_service_account.arn
 }
